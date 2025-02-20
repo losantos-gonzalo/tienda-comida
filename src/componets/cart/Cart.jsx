d1a9bcd411a59c42336977f708ae5050ea3dbb7f
gonzalo-losanotos <LosantosGonzalo@hotmail.com>
2025-02-20 17:45:23 -0300

    c6-m1.6

--- src/componets/cart/Cart.jsx
+++ src/componets/cart/Cart.jsx
@@ -1,13 +1,62 @@
 import { Flex } from '@chakra-ui/react'
-import React from 'react'
+import React, { useContext } from 'react'
+import Context from '../context/CartContext'
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+} from '@chakra-ui/react'
+
 
 const Cart = () => {
+    const { cart, removeItem, clearCart } = useContext(Context)
+    console.log('carrito', cart)
 
     return (
         <Flex>
-            <h2>
-                Carrito!
-            </h2>
+            <TableContainer>
+                <Table variant='striped' colorScheme='teal'>
+                    <TableCaption>Imperial to metric conversion factors</TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>Nombre</Th>
+                            <Th>Cantidad</Th>
+                            <Th>Precio</Th>
+                            <Th isNumeric>multiply by</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>inches</Td>
+                            <Td>millimetres (mm)</Td>
+                            <Td isNumeric>25.4</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>feet</Td>
+                            <Td>centimetres (cm)</Td>
+                            <Td isNumeric>30.48</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>yards</Td>
+                            <Td>metres (m)</Td>
+                            <Td isNumeric>0.91444</Td>
+                        </Tr>
+                    </Tbody>
+                    <Tfoot>
+                        <Tr>
+                            <Th>To convert</Th>
+                            <Th>into</Th>
+                            <Th isNumeric>multiply by</Th>
+                        </Tr>
+                    </Tfoot>
+                </Table>
+            </TableContainer>
         </Flex>
     )
 }
